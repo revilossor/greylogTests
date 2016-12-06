09b3d6f23e3a1df0fe5442cc046c941916e80a3f
Oliver <oliver.ross@dwp.gsi.gov.uk>
2016-12-06 10:55:29 +0000

    added gelf helper class

---
+++ app/greylog/GreylogComms.js
@@ -0,0 +1,12 @@
+export default class GreylogComms {
+  constructor() {
+    var Gelf = require('gelf');
+    this._gelf = new Gelf(require('./config');
+  }
+  sendMessage(message) {
+    this._gelf.emit('gelf.log', message);
+  }
+  sendData(data) {
+    this._gelf.emit('gelf.log', data);
+  }
+}
---
+++ app/greylog/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  graylogPort: 9000,
+  graylogHostname: '127.0.0.1',
+  connection: 'wan',
+  maxChunkSizeWan: 1420,
+  maxChunkSizeLan: 8154
+};
