4733e2973e1d28ce31682b8fed410156b84fd5bc
Oliver <oliver.ross@dwp.gsi.gov.uk>
2016-12-13 13:38:40 +0000

    removed request, added winston logger

--- app/graylog/graylogger.js
+++ app/graylog/graylogger.js
@@ -1,21 +1,7 @@
-var request = require('request');
 var config = require('./config');
 
 module.exports = {
   send:(content) => {
-    request.post({
-      url : `http://${config.host}:${config.port}/gelf`,
-      form : {
-        short_message:'from app',
-        host:'example.org',
-        facility:'test',
-        _foo:'bar'
-      }
-    }, function(err, res, status) {
-      if(err) {
-        return console.log('ERROR! : ' + JSON.stringify(err));
-      }
-      console.log(`response ${JSON.stringify(res)}`);
-    });
+    console.log('not sent content is : ' + JSON.stringify(content));
   }
 };
--- app/main.js
+++ app/main.js
@@ -2,15 +2,9 @@ var express = require('express');
 var app = express();
 var port = 8080;
 var graylogger = require('./graylog/graylogger');
-var gelf = require('./graylog/gelf');
 
 app.use('/test', (req, res) => {
-  var gelfy = gelf.get(req);
-
-  console.log('gelf to send is : ' + JSON.stringify(gelfy));
-
-  graylogger.send(gelf);
-
+  graylogger.send({some:"data"});
   res.status(200).json({some:"data"});
 });
 
