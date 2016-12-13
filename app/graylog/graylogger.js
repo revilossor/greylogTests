0846dab490291853a46b3fa45e9892d4e83ec23d
Oliver <oliver.ross@dwp.gsi.gov.uk>
2016-12-13 14:10:51 +0000

    gelf transport for winston sends to graylog container

--- app/graylog/graylogger.js
+++ app/graylog/graylogger.js
@@ -1,9 +1,17 @@
 var winston = require('winston');
+require('winston-log2gelf')
 
 winston.add(winston.transports.File, { filename: './log/app.log' });
+winston.add(winston.transports.Log2gelf, {
+  level: 'info',
+  host: 'graylog',
+  port: 12201,
+  protocol: 'http',
+  handleExceptions: true
+});
 
 module.exports = {
   send:(content) => {
-    winston.info('not sent content is : ' + JSON.stringify(content));
+    winston.info('log : ' + JSON.stringify(content));
   }
 };
